7a238469b27768d477a70651634a4bd99b518675
mrprince88 <mrprince0088@gmail.com>
2021-04-15 09:16:29 +0530

    fixed

--- vanillaCalendar.js
+++ vanillaCalendar.js
@@ -12,79 +12,88 @@
         pastDates: false,
         onSelect: (data, elem) => {
 
-        function add(t1, t2) {
-            let totalMins = t1.mins + t2.mins;
-            return new Time(Math.floor(totalMins / 60), totalMins % 60);
-        }
+            function add(t1, t2) {
+                let totalMins = t1.mins + t2.mins;
+                return new Time(Math.floor(totalMins / 60), totalMins % 60);
+            }
 
-        function roundOf(t, n) {
-            if (t.mins % 60 < n && (t.m != 15 || t.m != 30 || t.m != 0 || t.m != 45)) {
-                if (t.m < 15)
-                    t.m = 15;
-                else if (t.m < 30)
-                    t.m = 30;
-                else if (t.m < 45)
-                    t.m = 45;
-                else {
-                    t.m = 0;
-                    t.h++;
+            function roundOf(t1, n) {
+                let t = new Time(t1.h, t1.m);
+                if (t.m != 15 && t.m != 30 && t.m != 0 && t.m != 45) {
+                    if (t.m < 15)
+                        t.m = 15;
+                    else if (t.m < 30)
+                        t.m = 30;
+                    else if (t.m < 45)
+                        t.m = 45;
+                    else {
+                        t.m = 0;
+                        t.h++;
+                    }
+                    t.mins = t.h * 60 + t.m;
+                }
+                if ((t.mins - t1.mins) % n == 0)
+                    t = t1;
+                return t;
+            }
+
+            let avlTime = [
+                [new Time(8, 50), new Time(23, 00)],
+            ];
+
+            let slots = [],
+                interval = new Time(00, 10),
+                duration = new Time(00, 5),
+                n = avlTime.length;
+
+            for (let i = 0; i < n; i++) {
+                avlTime[i][0] = roundOf(avlTime[i][0], duration.mins);
+                while (avlTime[i][1].mins - avlTime[i][0].mins >= duration.mins) {
+                    let newTime = add(avlTime[i][0], duration);
+                    slots.push([avlTime[i][0], newTime]);
+                    avlTime[i][0] = add(avlTime[i][0], interval);
                 }
             }
-            t.mins = t.h * 60 + t.m;
-            return t;
-        }
 
-        let avlTime = [
-            [new Time(8, 50), new Time(9, 00)],
-            [new Time(14, 00), new Time(16, 00)]
-        ];
+            let events = document.querySelector('.events')
 
-        let slots = [],
-            interval = new Time(1, 00),
-            duration = new Time(00, 10),
-            n = avlTime.length;
+            n = slots.length;
 
-        for (let i = 0; i < n; i++) {
-            avlTime[i][0] = roundOf(avlTime[i][0], duration.mins);
-            while (avlTime[i][1].mins - avlTime[i][0].mins >= duration.mins) {
-                let newTime = add(avlTime[i][0], duration);
-                slots.push([avlTime[i][0], newTime]);
-                avlTime[i][0] = newTime;
+            for (let i = 0; i < n; i++) {
+                let a = document.createElement('button');
+                a.classList.add('block');
+                a.innerText = slots[i][0].h + ":" + slots[i][0].m;
+                a.setAttribute('id', i);
+                events.appendChild(a);
             }
-        }
-        n = slots.length;
-        let possible = [];
 
-        let curr = slots[0];
-        possible.push(curr);
-        for (let i = 1; i < n; i++) {
-            if (slots[i][0].mins - curr[0].mins >= interval.mins) {
-                curr = slots[i];
-                possible.push(curr);
+            let buttons = document.querySelectorAll('.block');
+
+            function flattenList() {
+                if (slots.length == 0)
+                    return slots;
+
+                let flat = [],
+                    prev = [new Time(slots[0][0].h, slots[0][0].m), new Time(slots[1][1].h, slots[1][1].m)];
+                flat.push(slots);
+                for (let i = 1; i < slots.length; i++) {
+                    let curr = [new Time(slots[i][0].h, slots[i][0].m), new Time(slots[i][1].h, slots[i][1].m)];
+
+                    if (prev[1].mins - curr[0].mins >= 0)
+                        prev[1] = curr[1];
+                    else {
+                        flat.push(curr);
+                        prev = curr;
+                    }
+                }
+                return flat;
             }
-        }
 
-        let events = document.querySelector('.events')
-        n = possible.length;
-
-        for (let i = 0; i < n; i++) {
-            let a = document.createElement('button');
-            a.classList.add('block');
-            a.innerText = possible[i][0].h + ":" + possible[i][0].m;
-            a.setAttribute('id', i);
-            events.appendChild(a);
-        }
-
-        let buttons = document.querySelectorAll('.block');
-        console.log(buttons);
-
-        buttons.forEach(i => {
-            i.addEventListener('click', () => {
-                possible.splice(i.id, 1);
-                console.log(possible);
-                i.remove()
+            buttons.forEach(i => {
+                i.addEventListener('click', () => {
+                    i.remove();
+                })
             })
-        })
-    }
+        }
     })
 }
